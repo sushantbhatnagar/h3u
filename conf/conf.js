cfb5dffe2407c847b98ffc7e630b3e48cfcfd3e2
Sushant Bhatnagar <sushantbhatnagar91@gmail.com>
2019-01-06 01:18:26 +0530

    Direct Connect feature added

--- conf/conf.js
+++ conf/conf.js
@@ -1,6 +1,6 @@
 exports.config={
-	// directConnect: true,
-	seleniumAddress: 'http://localhost:4444/wd/hub',
+	directConnect: true,
+	// seleniumAddress: 'http://localhost:4444/wd/hub',
 
 	capabilities:{
 		browserName: 'chrome',
