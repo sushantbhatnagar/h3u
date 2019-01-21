e3f4a86c9272730e7cd53448be9a44f86abbdae3
Sushant Bhatnagar <sushantbhatnagar91@gmail.com>
2019-01-21 13:14:43 +0530

    Added tests to check texts under Tools Trackers Page

--- pages/toolsTrackerPage.js
+++ pages/toolsTrackerPage.js
@@ -3,19 +3,30 @@ var Objects = require('../obj_repo/objects.json')
 
 var toolsTrackerPage = function(){
 
-	this.viewtoolsTrackers = function(){
-		element(by.xpath(Objects.homePageLocators.toolsTrackers)).click();
-		browser.sleep(30000);
 		/*
 			POM design pattern - write return this; if the function takes you to the same page reference
 			else give the reference of the next page...
 		*/
+	this.viewtoolsTrackers = function(){
+		element(by.xpath(Objects.homePageLocators.toolsTrackers)).click();
+		browser.sleep(30000);
 	};
 
 	this.selectedMemberValue = function(){
 		return element(by.model(Objects.toolsTrackersPageLocators.member)).getText();
 	};
 
+	this.bmiMetertext = function(){
+		element(by.xpath(Objects.toolsTrackersPageLocators.bmiMeter)).getText();
+	};
+
+	this.saltMetertext = function(){
+		element(by.xpath(Objects.toolsTrackersPageLocators.saltMeter)).getText();
+	};
+
+	this.smokeMetertext = function(){
+		element(by.xpath(Objects.toolsTrackersPageLocators.smokeMeter)).getText();
+	};
 };
 
 module.exports = new toolsTrackerPage();
--- test_spec/tools_trackers_h3u.js
+++ test_spec/tools_trackers_h3u.js
@@ -4,8 +4,6 @@ var loginPage = require('../pages/loginPage');
 var homePage = require('../pages/homePage');
 var toolsTrackerPage = require('../pages/toolsTrackerPage');
 var Objects = require('../obj_repo/objects.json');
-
-
 var logger = require('../logger/customlogger.js');
 
 // Feature File
@@ -18,18 +16,16 @@ describe('Tools and Trackers Validations', function(){
 		logger.info("Login in to the application...");
 		loginPage.loginToH3U();
 		logger.info("User is logged in successfully...");
-
 	});
 
 	it('I should be navigated to Tools and Trackers Page successfully', function(){
 		logger.info('Viewing Tools and Trackers Page...');
-		browser.debugger();
+		// browser.debugger();
 		toolsTrackerPage.viewtoolsTrackers();
 		logger.info("Navigated to Tools and Trackers Page successfully...")
 	});
 
-	it('"Select Member" dropdown should display the name of the policy holder along with its relation', function(){
-		
+	it('"Select Member" dropdown should display the name of the policy holder along with its relation', function(){		
 		logger.info('Viewing Tools and Trackers Page...');
 		toolsTrackerPage.viewtoolsTrackers();
 		logger.info("Navigated to Tools and Trackers Page successfully...")
@@ -42,5 +38,16 @@ describe('Tools and Trackers Validations', function(){
 		 expect(value).toContain(Objects.toolsTrackersPageValidations.memberName);
 		 expect(value).toContain('Self');
 		});
+	});
+
+	it('3 boxes with texts as BMI Meter, Salt Meter and Smoke Meter should be displayed', function(){
+		
+		logger.info('Validating 3 boxes should be displayed...');
+
+		expect(Objects.toolsTrackerPage.bmiMetertext()).toEqual(Objects.toolsTrackersPageValidations.bmiMeter);
+		expect(Objects.toolsTrackerPage.saltMetertext()).toEqual(Objects.toolsTrackersPageValidations.saltMeter);
+		expect(Objects.toolsTrackerPage.smokeMetertext()).toEqual(Objects.toolsTrackersPageValidations.smokeMeter);
+
+		logger.info('Validating 3 boxes are displayed...');
 	});
 });
