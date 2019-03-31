dda82e05a4d196e95a8820905101b30834beccc4
Sushant Bhatnagar <sushantbhatnagar91@gmail.com>
2019-03-31 14:56:43 +0530

    Added a scenario to count number of boxes on the homepage

--- test_spec/backbutton.js
+++ test_spec/backbutton.js
@@ -3,20 +3,22 @@ var loginPage = require('../pages/loginPage');
 var Objects = require('../obj_repo/objects.json');
 var base = require('../pages/basePage');
 
-
 describe('After Login', function(){
 
 	beforeEach(function(){
 		browser.get(Objects.h3utestSite);
+		loginPage.loginToH3U();
 	});
 
 	it('Clicks the back button', function(){
-		loginPage.loginToH3U();
-
 		element(by.xpath(Objects.backButtonPageLocators.logoutText)).getText().then(function(logoutText){
 			expect(logoutText).toEqual('Logout');
 		});
-	});
-		
-		// expect(element(by.buttonText(Objects.backButtonPageLocators.logoutText)).getText()).toEqual('Logout');
+
+	it('I should see 3 boxes visible on the homepage', function(){
+		element.all(by.className(Objects.backButtonPageLocators.threeBoxes)).then(function(totalBoxes){
+			expect(totalBoxes.length).toEqual (3);
+			});	
+		});
+	});	
 });
